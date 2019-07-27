5e90a1770c483fa83e9d2c15fa041405120173af
Sudipto Ghosh <sudipto@ghosh.pro>
2019-07-28 00:14:36 +0530

    added manipulators snippet

---
+++ Year I/Semester I/C++/Other/27-07-2019/manipulators/main.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+void manipGeneral()
+{
+    cout << setw(10) << 123 << endl;                 // prints seven spaces before 123
+    cout << setw(10) << setfill('*') << 123 << endl; // filss spaces with '*'
+}
+
+void manipFloat()
+{
+    cout << fixed << 1.2345 << endl;                        // precision defaults to 6
+    cout << setprecision(2) << 1.2345 << endl;              // precision set to 2
+    cout << setprecision(0) << 1.2345 << endl;              // only integral part
+    cout << setprecision(0) << showpoint << 1.2345 << endl; // shows the decimal point
+}
+
+int main()
+{
+    manipGeneral();
+    manipFloat();
+    cout << "\nExiting...\n";
+    return 0;
+}
