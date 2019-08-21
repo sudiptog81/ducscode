da1bea6014a18e7b71f445eb1dd4c2ea0e50ddd5
Sudipto Ghosh <sudipto@ghosh.pro>
2019-08-21 21:07:45 +0530

    rewritten valueReference

--- Year I/Semester I/C++/Other/19-08-2019/valueReference/main.cpp
+++ Year I/Semester I/C++/Other/19-08-2019/valueReference/main.cpp
@@ -2,10 +2,10 @@
 
 using namespace std;
 
-void swapValue(int a, int b);
-void swapReference(int &a, int &b);
-void changeVariableByValue(int a, int b);
-void changeVariableByReference(int &a, int &b);
+void swapValue(int, int);
+void swapReference(int &, int &);
+void changeVariableByValue(int, int);
+void changeVariableByReference(int &, int &);
 
 int main()
 {
@@ -14,22 +14,38 @@ int main()
     cin >> a;
     cout << "Enter B: ";
     cin >> b;
-
-    cout << "Initial Values:\n"
+    cout << endl;
+    cout << "Initial Values:\n---------------\n"
+         << "a: " << a << "\tb: " << b << endl;
+    cout << endl;
+    cout << "Swap by Value:\n---------------\n";
+    cout << "Before Call:\n"
          << "a: " << a << "\tb: " << b << endl;
     swapValue(a, b);
-    cout << "Swap by Value:\n"
+    cout << "After Call:\n"
+         << "a: " << a << "\tb: " << b << endl;
+    cout << endl;
+    cout << "Swap by Reference:\n---------------\n";
+    cout << "Before Call:\n"
          << "a: " << a << "\tb: " << b << endl;
     swapReference(a, b);
-    cout << "Swap by Reference:\n"
+    cout << "After Call:\n"
+         << "a: " << a << "\tb: " << b << endl;
+    cout << endl;
+    cout << "Change by Value:\n---------------\n";
+    cout << "Before Call:\n"
          << "a: " << a << "\tb: " << b << endl;
     changeVariableByValue(a, b);
-    cout << "Changed by Value:\n"
+    cout << "After Call:\n"
+         << "a: " << a << "\tb: " << b << endl;
+    cout << endl;
+    cout << "Change by Reference:\n---------------\n";
+    cout << "Before Call:\n"
          << "a: " << a << "\tb: " << b << endl;
     changeVariableByReference(a, b);
-    cout << "Changed by Reference:\n"
+    cout << "After Call:\n"
          << "a: " << a << "\tb: " << b << endl;
-
+    cout << endl;
     return 0;
 }
 
@@ -38,6 +54,8 @@ void swapValue(int a, int b)
     int temp = a;
     a = b;
     b = temp;
+    cout << "Inside Function:\n"
+         << "a: " << a << "\tb: " << b << endl;
     return;
 }
 
@@ -46,6 +64,8 @@ void swapReference(int &a, int &b)
     int temp = a;
     a = b;
     b = temp;
+    cout << "Inside Function:\n"
+         << "a: " << a << "\tb: " << b << endl;
     return;
 }
 
@@ -53,12 +73,16 @@ void changeVariableByValue(int a, int b)
 {
     a = 1;
     b = 2;
+    cout << "Inside Function:\n"
+         << "a: " << a << "\tb: " << b << endl;
     return;
 }
 
 void changeVariableByReference(int &a, int &b)
 {
     a = 1;
-    b = 4;
+    b = 2;
+    cout << "Inside Function:\n"
+         << "a: " << a << "\tb: " << b << endl;
     return;
 }
