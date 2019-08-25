53df82880af1a0b9672e0eaca0c8d6601b4354fc
Sudipto Ghosh <sudipto@ghosh.pro>
2019-08-25 11:34:19 +0530

    added idiomatic usage of vectors

---
+++ Year I/Semester I/C++/Other/25-08-2019/vectorIdiom/main.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main()
+{
+  int choice, n;
+
+  cout << "Choice - (1) Array Idiom (2) STL Idiom: ";
+  cin >> choice;
+
+  if (choice == 2)
+  {
+    cout << "Enter any number of integers & end with <EOF>: \n";
+
+    vector<int> ivec;
+
+    // populating the vector
+    while (cin >> n)
+    {
+      ivec.push_back(n);
+    }
+
+    // accessing the vector
+    for (vector<int>::iterator i = ivec.begin(); i != ivec.end(); i++)
+      cout << *i << " ";
+
+    ivec.clear();
+  } // Array Idiom
+
+  else if (choice == 1)
+  {
+    int size;
+    cout << "Enter the size of the vector: ";
+    cin >> size;
+
+    vector<int> ivec(size);
+
+    // populating the vector
+    for (int i = 0; i < ivec.size(); i++)
+    {
+      cin >> n;
+      ivec[i] = n;
+    }
+
+    // accessing the vector
+    for (int i = 0; i < ivec.size(); i++)
+      cout << ivec[i] << " ";
+
+    ivec.clear();
+  } // STL Idiom
+
+  cout << endl;
+
+  return 0;
+}
