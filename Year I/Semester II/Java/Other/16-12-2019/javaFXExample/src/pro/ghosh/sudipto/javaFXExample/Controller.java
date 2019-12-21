1a703854483b8c4e36cfc3a4ca89efa9b3c3ee85
Sudipto Ghosh <sudipto@ghosh.pro>
2019-12-21 10:52:22 +0530

    Tinkered a little with FXML
    Signed-off-by: Sudipto Ghosh <sudipto@ghosh.pro>

--- Year I/Semester II/Java/Other/16-12-2019/javaFXExample/src/pro/ghosh/sudipto/javaFXExample/Controller.java
+++ Year I/Semester II/Java/Other/16-12-2019/javaFXExample/src/pro/ghosh/sudipto/javaFXExample/Controller.java
@@ -1,7 +1,20 @@
 package pro.ghosh.sudipto.javaFXExample;
 
-public class Controller {
-    void close() {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.text.Text;
+import javafx.scene.control.PasswordField;
 
+public class Controller {
+    @FXML
+    private Text actionTarget;
+
+    @FXML
+    private PasswordField passwordTarget;
+
+    @FXML
+    protected void handleSubmitButtonAction(ActionEvent event) {
+        System.out.println("Password: " + passwordTarget.getText());
+        actionTarget.setText("Sign in button pressed");
     }
 }
