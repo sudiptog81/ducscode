4a0accefab93de710814bfdb1a9fe3fcb15990ba
Sudipto Ghosh <sudipto@ghosh.pro>
2020-06-23 11:37:18 +0530

    replaced anon classes with lambdas
    Signed-off-by: Sudipto Ghosh <sudipto@ghosh.pro>

--- YearI/SemesterII/Java/Practicals/26-03-2020/SwingDetGPA/CGPA.java
+++ YearI/SemesterII/Java/Practicals/26-03-2020/SwingDetGPA/CGPA.java
@@ -1,31 +1,28 @@
 
 /**** CGPA.java ****/
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
 
 class CGPA {
-    JLabel l;
-    JButton btnClose;
+	JLabel l;
+	JButton btnClose;
 
-    CGPA(String cgpa) {
-        JFrame f = new JFrame("Previous Year CGPA");
-        l = new JLabel("Your CGPA was: " + cgpa, SwingConstants.CENTER);
-        l.setBounds(20, 20, 250, 30);
-        btnClose = new JButton("Close");
-        btnClose.setBounds(90, 60, 100, 30);
-        btnClose.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                f.dispose();
-            }
-        });
-        f.add(l);
-        f.add(btnClose);
-        f.setSize(310, 150);
-        f.setLayout(null);
-        f.setVisible(true);
-        f.setLocationRelativeTo(null);
-        f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-    }
+	CGPA(String cgpa) {
+		JFrame f = new JFrame("Previous Year CGPA");
+		this.l = new JLabel("Your CGPA was: " + cgpa, SwingConstants.CENTER);
+		this.l.setBounds(20, 20, 250, 30);
+		this.btnClose = new JButton("Close");
+		this.btnClose.setBounds(90, 60, 100, 30);
+		this.btnClose.addActionListener(i -> f.dispose());
+		f.add(this.l);
+		f.add(this.btnClose);
+		f.setSize(310, 150);
+		f.setLayout(null);
+		f.setVisible(true);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+	}
 }
--- YearI/SemesterII/Java/Practicals/26-03-2020/SwingDetGPA/Main.java
+++ YearI/SemesterII/Java/Practicals/26-03-2020/SwingDetGPA/Main.java
@@ -1,53 +1,53 @@
 
 /**
- * Using Swing, write a program to create two buttons labelled 
- * ‘A’ and ‘B’. When button ‘A’ is pressed, it displays your 
- * personal information (Name, Course, Roll No, College) and 
- * when button ‘B’ is pressed, it displays your CGPA in 
+ * Using Swing, write a program to create two buttons labelled
+ * 'A' and 'B'. When button 'A' is pressed, it displays your
+ * personal information (Name, Course, Roll No, College) and
+ * when button 'B' is pressed, it displays your CGPA in
  * previous semester.
  *
  * @author sudipto@ghosh.pro University of Delhi
  */
 
 /**** Main.java ****/
-import java.awt.*;
-import java.awt.event.*;
-import javax.swing.*;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.SwingUtilities;
 
 public class Main implements ActionListener {
-    JButton btnInfo, btnCGPA;
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> new Main());
+	}
 
-    Main() {
-        JFrame f = new JFrame("Student Details");
-        btnInfo = new JButton("A");
-        btnInfo.setBounds(18, 100, 450, 100);
-        btnInfo.addActionListener(this);
-        f.add(btnInfo);
-        btnCGPA = new JButton("B");
-        btnCGPA.setBounds(18, 270, 450, 100);
-        btnCGPA.addActionListener(this);
-        f.add(btnCGPA);
-        f.setSize(500, 500);
-        f.setLayout(null);
-        f.setVisible(true);
-        f.setLocationRelativeTo(null);
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    }
+	JButton btnInfo, btnCGPA;
 
-    public static void main(String[] args) {
-        SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                new Main();
-            }
-        });
-    }
+	Main() {
+		JFrame f = new JFrame("Student Details");
+		this.btnInfo = new JButton("A");
+		this.btnInfo.setBounds(18, 100, 450, 100);
+		this.btnInfo.addActionListener(this);
+		f.add(this.btnInfo);
+		this.btnCGPA = new JButton("B");
+		this.btnCGPA.setBounds(18, 270, 450, 100);
+		this.btnCGPA.addActionListener(this);
+		f.add(this.btnCGPA);
+		f.setSize(500, 500);
+		f.setLayout(null);
+		f.setVisible(true);
+		f.setLocationRelativeTo(null);
+		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	}
 
-    @Override
-    public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == btnInfo) {
-            new Information("SUDIPTO GHOSH", "BSc (Hons) Computer Science", "19/78003", "ARSD College");
-        } else if (e.getSource() == btnCGPA) {
-            new CGPA("9.73");
-        }
-    }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		if (e.getSource() == this.btnInfo) {
+			new Information("SUDIPTO GHOSH", "BSc (Hons) Computer Science", "19/78003", "ARSD College");
+		} else if (e.getSource() == this.btnCGPA) {
+			new CGPA("9.73");
+		}
+	}
 }
