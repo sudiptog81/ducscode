b12a8a44054708731954ac1f778bba0dc35aa1db
Sudipto Ghosh <sudipto@ghosh.pro>
2020-08-11 13:28:26 +0530

    added submodule for ds-labs

--- YearI/SemesterII/Java/Practicals/23-01-2020/LexAnalyzer/LexAnalyzer.java
+++ YearI/SemesterII/Java/Practicals/23-01-2020/LexAnalyzer/LexAnalyzer.java
@@ -12,31 +12,31 @@ import java.io.*;
 // Sudipto Ghosh
 
 public class LexAnalyzer {
-    static boolean analyze(String line) {
-        if (line.length() >= 2)
-            return line.substring(0, 2).equals("//");
-        return false;
-    }
+	static boolean analyze(String line) {
+		if (line.length() >= 2)
+			return line.substring(0, 2).equals("//");
+		return false;
+	}
 
-    public static void main(String[] args) {
-        if (args.length != 1) {
-            System.err.println("Usage: java LexAnalyzer <file>");
-        } else {
-            try {
-                String str;
-                BufferedReader br = new BufferedReader(new FileReader(args[0]));
-                while ((str = br.readLine()) != null) {
-                    str = str.trim();
-                    if (analyze(str)) {
-                        System.out.println(str);
-                    }
-                }
-                br.close();
-            } catch (Exception e) {
-                System.err.println(e.getMessage());
-            }
-        }
-    }
+	public static void main(String[] args) {
+		if (args.length != 1) {
+			System.err.println("Usage: java LexAnalyzer <file>");
+		} else {
+			try {
+				String str;
+				BufferedReader br = new BufferedReader(new FileReader(args[0]));
+				while ((str = br.readLine()) != null) {
+					str = str.trim();
+					if (analyze(str)) {
+						System.out.println(str);
+					}
+				}
+				br.close();
+			} catch (Exception e) {
+				System.err.println(e.getMessage());
+			}
+		}
+	}
 }
 
 // This is a line of comment
